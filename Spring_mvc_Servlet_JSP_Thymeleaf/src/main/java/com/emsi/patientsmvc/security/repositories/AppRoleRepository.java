b2e96bdbe39b2b6423fd705f2f99290a5b1bbc52
Kawtarfahmy <88769633+Kawtarfahmy@users.noreply.github.com>
2022-04-28 02:09:24 +0200

    Delete Spring_mvc_Servlet_JSP_Thymeleaf directory

--- Spring_mvc_Servlet_JSP_Thymeleaf/src/main/java/com/emsi/patientsmvc/security/repositories/AppRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.emsi.patientsmvc.security.repositories;
-
-import com.emsi.patientsmvc.security.entities.AppRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppRoleRepository extends JpaRepository<AppRole, Long> {
-    AppRole findByRoleName(String roleName);
-}
--- Spring_mvc_Servlet_JSP_Thymeleaf/src/main/java/com/emsi/patientsmvc/security/repositories/AppUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.emsi.patientsmvc.security.repositories;
-
-import com.emsi.patientsmvc.security.entities.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppUserRepository extends JpaRepository<AppUser, String> {
-    AppUser findByUsername(String username);
-}
